215c00c5fd287c00ed6b13dae79e03c4280bb7a1
Jose Luis Mejia Laguna <jlml@joses-mbp.mynetworksettings.com>
2024-02-08 12:36:43 -0500

    fixing vercel env undefined return issue

--- src/helpers/getEnvironmentVar.ts
+++ src/helpers/getEnvironmentVar.ts
@@ -1,9 +1,3 @@
-export const getEnvironmentVar = (token: string): string | undefined => {
-  const envToken = import.meta.env[token];
-  if (token && envToken) {
-    return envToken;
-  } else {
-    console.error(`Could not find environment variable, this ${token} does not exist`)
-    return undefined;
-  }
+export const getEnvironmentVar = (token: string): string => {
+  return import.meta.env[token];
 };
